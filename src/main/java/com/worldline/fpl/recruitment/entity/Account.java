3e4e9cb2672d185dfea753853c7846a421c72e36
CHARAF EL BAROUDY <CElbaroudi@accolade.co.ma>
2017-03-06 19:10:32 +0100

    feature(installation): Installation,running the project and adding getters, setters, builders using lombok

--- src/main/java/com/worldline/fpl/recruitment/entity/Account.java
+++ src/main/java/com/worldline/fpl/recruitment/entity/Account.java
@@ -4,7 +4,12 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Account entity
@@ -13,19 +18,35 @@ import lombok.Data;
  *
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Account implements Serializable {
 
 	private static final long serialVersionUID = -3548441891975414771L;
 
+	@Getter
+	@Setter
 	private String id;
 
+	@Getter
+	@Setter
 	private String number;
 
+	@Getter
+	@Setter
 	private String type;
 
+	@Getter
+	@Setter
 	private BigDecimal balance;
 
+	@Getter
+	@Setter
 	private Date creationDate;
 
+	@Getter
+	@Setter
 	private boolean isActive;
+
 }
--- src/main/java/com/worldline/fpl/recruitment/json/ErrorCode.java
+++ src/main/java/com/worldline/fpl/recruitment/json/ErrorCode.java
@@ -2,6 +2,7 @@ package com.worldline.fpl.recruitment.json;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import org.springframework.http.HttpStatus;
 
@@ -17,5 +18,6 @@ public enum ErrorCode {
 	INVALID_ACCOUNT(HttpStatus.NOT_FOUND);
 
 	@Getter
+	@Setter
 	private HttpStatus httpStatus;
 }
